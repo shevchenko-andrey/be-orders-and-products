84836c6eb514f9c979c2e01c89c7e35e056f540f
andrew <s.s.andriy@student.khai.edu>
2023-05-05 21:55:27 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(8080);
   app.useWebSocketAdapter(new IoAdapter(app));
+  app.enableCors({ credentials: true, origin: process.env.FRONTEND_URL });
+  await app.listen(8080);
 }
 bootstrap();
